14d20d47feca8d16841da4b849a31b91ed8c5742
Salvanim <dylanfish05@icloud.com>
2024-08-27 12:22:09 -0400

    CaseStudy

---
+++ CaseStudy/study.py
@@ -0,0 +1,48 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+#pulls data form college grad and poverty rate csv files
+collegeGrad = pd.read_csv("college_graduates.csv")
+povertyRate = pd.read_csv("poverty_rate.csv")
+
+#appends data from college grad names and changes to arrays
+collegeGradCounty = []
+collegeGradState = []
+collegeChange = []
+
+for row in collegeGrad.iterrows():
+  if str(row[1]["Change_in_Fraction_of_College_Graduates"]) != 'nan' and str(row[1]['Name']) != 'nan':
+    #filters out empty values
+    collegeGradCounty.append(str(row[1]['Name']).split(", ")[0])
+    collegeGradState.append(str(row[1]['Name']).split(", ")[1])
+    collegeChange.append(row[1]["Change_in_Fraction_of_College_Graduates"])
+
+#appends data from poverty rate names and changes to arrays
+povertyRateCounty = []
+povertyRateState = []
+povertyChange = []
+
+for row in povertyRate.iterrows():
+  #filters out empty values
+  if str(row[1]["Change_in_Poverty_Rate"]) != 'nan' and str(row[1]['Name']) != 'nan':
+    povertyRateCounty.append(str(row[1]['Name']).split(", ")[0])
+    povertyRateState.append(str(row[1]['Name']).split(", ")[1])
+    povertyChange.append(row[1]["Change_in_Poverty_Rate"])
+
+#Converts arrays back to dataframes
+collegeGrad = pd.DataFrame.from_dict({
+    "County": collegeGradCounty,
+    "State" : collegeGradState,
+    "Change": collegeChange
+})
+
+povertyRate = pd.DataFrame.from_dict({
+    "County": povertyRateCounty,
+    "State" : povertyRateState,
+    "Change": povertyChange
+})
+
+collegeGrad.iloc[:10].plot(x="County", y="Change", kind="bar")
+plt.show()
